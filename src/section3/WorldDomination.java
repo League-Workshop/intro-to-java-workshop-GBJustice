6dc506b0fe3cf195d7e685cf6b06b2a891965d51
League Student <league@iMac-7.attlocal.net>
2019-07-17 14:28:15 -0700

    DAY 3

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,17 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		String name = JOptionPane.showInputDialog("Do you play fortnite?");
+		if(name.equals("Yes")) {
+			JOptionPane.showMessageDialog(null, "Nice, your a hacker");
+		}
 		// 2. If they say "yes", tell them they will rule the world.
+		else {JOptionPane.showMessageDialog(null, "Good luck washing dishes");
+		
 
 		// 3. Otherwise, wish them good luck washing dishes.
-
+			
+		
 	}
 }
-
+}
